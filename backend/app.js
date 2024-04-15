a57454329c7a7e3c14789aaf6576528cf9cc6317
Alvaro Avila <Alvaro.Avila1@alu.uclm.es>
2024-04-15 10:55:10 +0200

    enrutado de jugador

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var jugadoresRouter = require('./routes/jugadores');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/jugadores', jugadoresRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
