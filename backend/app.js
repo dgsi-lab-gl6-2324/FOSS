98cf0089511b7cf4bc15cbb6ba18a29480118d82
Alvaro Avila <Alvaro.Avila1@alu.uclm.es>
2024-04-15 10:37:40 +0200

    añadida dependencia mongoose y conexion con MongoDB

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB)
+  .then(() => {
+  }).catch(err => {
+    console.log(err)
+  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
