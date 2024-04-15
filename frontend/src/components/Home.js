026e47837b06d5d58b65d2fdac8c28d8b7b5a64f
Daniel <daniel.lopez46@alu.uclm.es>
2024-04-15 09:11:28 +0200

    agregado slider y composicion de pantalla Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,31 @@
-import { Button } from "reactstrap";
+import React from "react";
+import { Button, Container, Row } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Container } from "reactstrap";
+import Slider from "./Slider";
+
 const Home = () => {
   return (
-    <Container>
-      <Link to="/players">
-        <Button>PLAYERS</Button>
-      </Link>
-      <Link to="/teams">
-        <Button>TEAMS</Button>
-      </Link>
+    <Container className="d-flex flex-column align-items-center justify-content-center">
+      <Row className="text-center mt-3">
+        <h4 className="display-9">
+          Bienvenido al sistema de soporte de clubes de Fútbol
+        </h4>
+      </Row>
+      <Row className="mt-4">
+        <Slider />
+      </Row>
+      <Row className="d-flex justify-content-center mt-3">
+        <Link to="/players" className="text-decoration-none">
+          <Button color="primary" className="">
+            VER JUGADORES
+          </Button>
+        </Link>
+      </Row>
+      <Row className="d-flex justify-content-center mt-2">
+        <Link to="/teams" className="text-decoration-none">
+          <Button color="success">VER EQUIPOS</Button>
+        </Link>
+      </Row>
     </Container>
   );
 };
