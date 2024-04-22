f047e43a9c9b549a5a370c95a7b7a83023df6175
Alvaro Avila <Alvaro.Avila1@alu.uclm.es>
2024-04-22 12:16:21 +0200

    enrutado equipos

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jugadoresRouter = require('./routes/jugadores');
+var equiposRouter = require('./routes/equipos');
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jugadores', jugadoresRouter);
+app.use('/equipos', equiposRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
