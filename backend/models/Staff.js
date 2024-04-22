b31a23dbb82f901c30a8bfb14f8cfa5c00634c53
Alvaro Avila <Alvaro.Avila1@alu.uclm.es>
2024-04-23 00:26:01 +0200

    selector de categorias y modelo Staff

---
+++ backend/models/Staff.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var StaffSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido1: String,
+    apellido2: String,
+    edad: Number,
+    email: String,
+    telefono: String,
+    direccion: String,
+    ciudad: String,
+    provincia: String,
+    zip: Number,
+    equipo: {
+        type: Schema.Types.ObjectId,
+        ref: 'Equipo'
+    },
+    rol: {
+        type: String,
+        enum: [
+            'entrenadr',
+            'entrenador2',
+            'entrenadorPorteros',
+            'delegado',
+            'fisioterapeuta',
+            'preparador',
+            'medico'
+        ],
+        required: true
+    },
+    titulo: String
+}, { collection: 'staff' });
+
+module.exports = mongoose.model('Staff', StaffSchema);
