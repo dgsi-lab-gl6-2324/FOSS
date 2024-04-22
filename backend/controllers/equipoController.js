c3a3f1388f2a3aac363355adb60574ddfd77ad05
Alvaro Avila <Alvaro.Avila1@alu.uclm.es>
2024-04-23 01:05:33 +0200

    POST de equipo incluye staff

--- backend/controllers/equipoController.js
+++ backend/controllers/equipoController.js
@@ -1,6 +1,7 @@
 var debug = require('debug')('backend:jugadoresController');
 var Equipo = require('../models/Equipo');
 var Jugador = require('../models/Jugador');
+var Staff = require('../models/Staff');
 
 /* GET lista equipos */
 exports.list = (req, res) => {
@@ -33,34 +34,50 @@ exports.new = (req, res) => {
     var nuevoEquipo = new Equipo({
         nombre: req.body.nombre,
         categoria: req.body.categoria,
-        jugadores: req.body.jugadores ? req.body.jugadores : []
-        // TODO: añadir staff
+        jugadores: req.body.jugadores ? req.body.jugadores : [],
+        staff: req.body.staff ? req.body.staff : []
     });
     nuevoEquipo.save()
         .then(equipo => {
+            let promises = [];
+
             if (equipo.jugadores.length > 0) {
-                // Recorre la lista de jugadores y actualiza cada uno de ellos
-                let promises = equipo.jugadores.map(jugadorId => {
+                // Actualiza los jugadores
+                promises.push(...equipo.jugadores.map(jugadorId => {
                     return Jugador.findById(jugadorId).exec()
                         .then(jugador => {
                             if (!jugador) {
-                                res.status(404).json({message: "Jugador no encontrado"});
-                                debug("POST /equipos ERROR");
+                                res.status(404).json({ message: "Jugador no encontrado" });
+                                debug("POST /equipos ERROR jugadaor no encontrado");
                             }
                             jugador.equipo = equipo._id;
                             return jugador.save();
                         });
-                });
-                // Espera a que todas las promesas se resuelvan
-                return Promise.all(promises)
-                    .then(() => {
-                        res.status(201).json(equipo);
-                        debug("POST /equipos");
-                    });
-            } else {
-                res.status(201).json(equipo);
-                debug("POST /equipos");
+                }));
             }
+
+            if (equipo.staff.length > 0) {
+                // Actualiza el staff
+                promises.push(...equipo.staff.map(staffId => {
+                    return Staff.findById(staffId).exec()
+                        .then(staff => {
+                            if (!staff) {
+                                res.status(404).json({ message: "Staff no encontrado" });
+                                debug("POST /equipos ERROR staff no encontrado");
+                            }
+                            staff.equipo = equipo._id;
+                            debug("POST a equipo %s con el staff %s", equipo._id, staff._id);
+                            return staff.save();
+                        });
+                }));
+            }
+
+            // Espera a que todas las promesas se resuelvan
+            return Promise.all(promises)
+                .then(() => {
+                    res.status(201).json(equipo);
+                    debug("POST /equipos");
+                });
         })
         .catch(err => {
             res.status(500).json(err);
