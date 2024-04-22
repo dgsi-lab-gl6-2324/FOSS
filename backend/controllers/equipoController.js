148fac3d37e0547ab6c4f9935b8e46989e1984c4
Alvaro Avila <Alvaro.Avila1@alu.uclm.es>
2024-04-23 00:23:40 +0200

    POST equipos añade equipo al jugador

--- backend/controllers/equipoController.js
+++ backend/controllers/equipoController.js
@@ -1,5 +1,6 @@
 var debug = require('debug')('backend:jugadoresController');
 var Equipo = require('../models/Equipo');
+var Jugador = require('../models/Jugador');
 
 /* GET lista equipos */
 exports.list = (req, res) => {
@@ -29,11 +30,36 @@ exports.show = (req, res) => {
 
 /* POST nuevo equipo */
 exports.new = (req, res) => {
-    var nuevoEquipo = new Equipo(req.body);
+    var nuevoEquipo = new Equipo({
+        nombre: req.body.nombre,
+        categoria: req.body.categoria,
+        jugadores: req.body.jugadores ? req.body.jugadores : []
+    });
     nuevoEquipo.save()
         .then(equipo => {
-            res.status(201).json(equipo);
-            debug("POST /equipos");
+            if (equipo.jugadores.length > 0) {
+                // Recorre la lista de jugadores y actualiza cada uno de ellos
+                let promises = equipo.jugadores.map(jugadorId => {
+                    return Jugador.findById(jugadorId).exec()
+                        .then(jugador => {
+                            if (!jugador) {
+                                res.status(404).json({message: "Jugador no encontrado"});
+                                debug("POST /equipos ERROR");
+                            }
+                            jugador.equipo = equipo._id;
+                            return jugador.save();
+                        });
+                });
+                // Espera a que todas las promesas se resuelvan
+                return Promise.all(promises)
+                    .then(() => {
+                        res.status(201).json(equipo);
+                        debug("POST /equipos");
+                    });
+            } else {
+                res.status(201).json(equipo);
+                debug("POST /equipos");
+            }
         })
         .catch(err => {
             res.status(500).json(err);
